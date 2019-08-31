80daedb5b56c94bf80bc29f1fdbc6356fad9b556
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-08-31 20:15:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -46,6 +46,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array = ["wow", "I", "am", "really", "totally", "arrays!"]
 array[4]
 end
