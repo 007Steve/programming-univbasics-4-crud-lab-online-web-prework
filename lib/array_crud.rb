3ca32756afbf8556ed1c6f4f77b0bf12d4ce72a8
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2019-08-31 19:59:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ end
 
 def remove_element_from_end_of_array(array)
    array = ["wow", "I", "am", "really", "learning", "arrays!"]
-   arrays= arrays.pop
+   arrays! = arrays.pop
 end
 
 def remove_element_from_start_of_array(array)
